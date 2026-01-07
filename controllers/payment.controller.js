3a0df7a3a5188bc0ce60a354b87dd18126a8f9c5
co-den <iamagugbueikenna@gmail.com>
2026-01-07 10:18:32 +0100

    added paymentLink Controller

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,4 +1,4 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const PaymentLink = require("../models/PaymentLinkModel");
 const Transaction = require("../models/Transaction.model");
 
